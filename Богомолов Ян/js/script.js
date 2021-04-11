479ef89874d0ac3cef9a9d785973f9188070214f
yan-create <77582131+yan-create@users.noreply.github.com>
2021-04-11 19:26:22 +0300

    Add files via upload

--- Богомолов Ян/js/script.js
+++ Богомолов Ян/js/script.js
@@ -30,3 +30,53 @@ $(document).ready(function(){
         defaultOpen: ''
     });
 });
+
+$(function(){
+    let filterList = {
+        init: function(){
+            $('.workportfolio').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap'
+            });
+        },
+    };
+    filterList.init();
+});
+
+$(document).ready(function($){
+    $('.primary-nav-trigger').on('click', function(){
+        $('.menu-icon').toggleClass('is-clicked');
+        $('.primary-nav').toggleClass('is-visible');
+        $('body').toggleClass('overflow-hidden');
+    });
+});
+
+$('.primary-nav').on("click","a", function(event){
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body, html').animate({scrollTop: top}, 1000);
+    
+    $('.primary-nav').toggleClass('is-visible');
+    $('.menu-icon').toggleClass('is-clicked');
+    $('body').toggleClass('overflow-hidden');
+});
+
+let headerHeight = $(".topheader").height();
+$(window).on("scroll", {previousTop:0},
+    function(){
+        let currentTop = $(window).scrollTop();
+        if (currentTop<this.previousTop) {
+            if (!(currentTop>0) && $(".topheader").hasClass("is-fixed")){
+                $(".topheader").removeClass("is-fixed");
+            }
+        } else {
+            if (currentTop>headerHeight && !$(".topheader").hasClass("is-fixed")){
+                $(".topheader").addClass("is-fixed");
+            }
+        }
+        this.previousTop = currentTop;
+
+    });
